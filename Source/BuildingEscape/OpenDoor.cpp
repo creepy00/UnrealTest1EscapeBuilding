998fb43805be62024fe26eb7144dd3680632b502
Goran <goran.blazin@gmail.com>
2019-01-09 01:12:13 +0100

    Finished building room

--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -1,32 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
-#include "Gameframework/Actor.h"
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-    
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-    FString ObjectName = GetOwner()->GetName();
-    FVector ObjectPos = GetOwner()->GetActorLocation();
 
-	UE_LOG(LogTemp, Warning, TEXT("%s is at position %s"), *ObjectName, *(ObjectPos.ToString()));
+	AActor* Owner = GetOwner();
+
+	Owner->SetActorRotation(FRotator(0.f, 120.f, 0.f));
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
--- Source/BuildingEscape/PositionReportMonument.cpp
+++ Source/BuildingEscape/PositionReportMonument.cpp
@@ -1,32 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
+#include "PositionReportMonument.h"
 #include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UPositionReportMonument::UPositionReportMonument()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-    
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UPositionReportMonument::BeginPlay()
 {
 	Super::BeginPlay();
-    FString ObjectName = GetOwner()->GetName();
-    FVector ObjectPos = GetOwner()->GetActorLocation();
+	FString ObjectName = GetOwner()->GetName();
 
-	UE_LOG(LogTemp, Warning, TEXT("%s is at position %s"), *ObjectName, *(ObjectPos.ToString()));
+    UE_LOG(LogTemp, Error, TEXT("Position report on %s ready!!"), *ObjectName);
+	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UPositionReportMonument::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
